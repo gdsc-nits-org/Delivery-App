9658f5dcaa9f74db7b81a6f933cd93389db2abee
AnKiTk203 <kumar0ankit2003@gmail.com>
2024-01-24 03:52:54 +0530

    Updating

--- app/src/main/java/com/example/deliveryapp/UserProfileActivity.kt
+++ app/src/main/java/com/example/deliveryapp/UserProfileActivity.kt
@@ -1,12 +1,21 @@
 package com.example.deliveryapp
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.deliveryapp.userprofile.ProfileListFragment
+
 
 class UserProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fvFragment, ProfileListFragment())
+        fragmentTransaction.commit()
 
     }
 }
