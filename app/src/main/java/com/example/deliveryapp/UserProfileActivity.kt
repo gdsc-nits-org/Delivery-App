dc956af304d8e9b9867e928e44bf969239b2e6dd
AnKiTk203 <kumar0ankit2003@gmail.com>
2024-02-01 05:25:07 +0530

    Updating

--- app/src/main/java/com/example/deliveryapp/UserProfileActivity.kt
+++ app/src/main/java/com/example/deliveryapp/UserProfileActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
-import com.example.deliveryapp.userprofile.EditProfileFragment
 import com.example.deliveryapp.userprofile.ProfileListFragment
 
 
