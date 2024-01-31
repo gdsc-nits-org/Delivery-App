eedc9057ce7787f08e5a2a5ea5e4e54f5cf1f51d
AnKiTk203 <kumar0ankit2003@gmail.com>
2024-02-01 05:29:42 +0530

    Updating

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "com.example.deliveryapp"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
