747251423deaa07ffb7ecb195ed35ef7c2fa741f
devmeh19 <devmehrotra@gmail.com>
2024-03-28 23:29:49 +0530

    Cart

---
+++ app/src/main/java/com/example/addtocart/Successful.kt
@@ -0,0 +1,11 @@
+package com.example.addtocart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Successful : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_successful)
+    }
+}
