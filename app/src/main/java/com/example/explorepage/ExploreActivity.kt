e77dc5186d2fcad27f9d273d11f2f88ea6996843
Vishal Singh <vishal684pratap@gmial.com>
2024-03-29 22:41:52 +0530

    Explore Page

--- app/src/main/java/com/example/explorepage/ExploreActivity.kt
+++ app/src/main/java/com/example/explorepage/ExploreActivity.kt
@@ -3,9 +3,9 @@ package com.example.explorepage
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ExploreActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_explore)
     }
 }
