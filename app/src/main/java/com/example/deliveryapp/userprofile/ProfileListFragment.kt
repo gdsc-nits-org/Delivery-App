56d6a72cb3cb0c54c0ed9748c152f12b07fef33c
AnKiTk203 <kumar0ankit2003@gmail.com>
2024-06-05 20:44:15 +0530

    LogOut Added

--- app/src/main/java/com/example/deliveryapp/userprofile/ProfileListFragment.kt
+++ app/src/main/java/com/example/deliveryapp/userprofile/ProfileListFragment.kt
@@ -20,23 +20,7 @@ import com.google.firebase.auth.FirebaseAuth
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
