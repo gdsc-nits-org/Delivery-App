ebdac719f2a23a98895d24db92aa306c6727399e
AnKiTk203 <kumar0ankit2003@gmail.com>
2024-06-05 20:44:29 +0530

    LogOut Added

--- app/src/main/java/com/example/deliveryapp/userprofile/ProfileListFragment.kt
+++ app/src/main/java/com/example/deliveryapp/userprofile/ProfileListFragment.kt
@@ -15,10 +15,6 @@ import com.example.deliveryapp.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 class ProfileListFragment : Fragment() {
 
