0731e9d62de5ed04d0e3f7df2745f7b4a77c800c
Bishal Paul Biswas <bishalp.biswas@gmail.com>
2024-06-13 01:42:51 +0530

    dialog box added for asking notifications permissions

--- app/src/main/java/com/example/deliveryapp/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/deliveryapp/MyFirebaseMessagingService.kt
@@ -13,54 +13,51 @@ import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 const val channelId = "notification_channel"
-const val channelName="com.example.deliveryapp"
+const val channelName = "com.example.deliveryapp"
 
 @SuppressLint("MissingFirebaseInstanceTokenRefresh")
-class MyFirebaseMessagingService : FirebaseMessagingService(){
+class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
 
-        if(remoteMessage.getNotification()!= null){
+        if (remoteMessage.notification != null) {
             generateNotification(remoteMessage.notification!!.title!!, remoteMessage.notification!!.body!!)
         }
     }
 
-    fun getRemoteView(title: String,message: String): RemoteViews{
-        val remoteView = RemoteViews("com.example.deliveryapp",R.layout.notification)
+    private fun getRemoteView(title: String, message: String): RemoteViews {
+        val remoteView = RemoteViews("com.example.deliveryapp", R.layout.notification)
 
-        remoteView.setTextViewText(R.id.head,title)
-        remoteView.setTextViewText(R.id.message,message)
-        remoteView.setImageViewResource(R.id.app_logo,R.drawable.logo__1_)
+        remoteView.setTextViewText(R.id.head, title)
+        remoteView.setTextViewText(R.id.message, message)
+        remoteView.setImageViewResource(R.id.app_logo, R.drawable.logo__1_)
 
         return remoteView
     }
 
-    fun generateNotification(title: String, message:String){
+    private fun generateNotification(title: String, message: String) {
 
-        val intent= Intent(this, MainActivity::class.java)
+        val intent = Intent(this, MainActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
 
-        val pendingIntent= PendingIntent.getActivity(this,0,intent,
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent,
             PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE)
 
-        var builder: NotificationCompat.Builder = NotificationCompat.Builder(applicationContext, channelId)
+        val builder: NotificationCompat.Builder = NotificationCompat.Builder(applicationContext, channelId)
             .setSmallIcon(R.drawable.logo__1_)
             .setAutoCancel(true)
-            .setVibrate(longArrayOf(1000,1000,1000,1000))
+            .setVibrate(longArrayOf(1000, 1000, 1000, 1000))
             .setOnlyAlertOnce(true)
             .setContentIntent(pendingIntent)
-
-        builder=builder.setContent(getRemoteView(title,message))
+            .setCustomContentView(getRemoteView(title, message))
 
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            val notificationChannel= NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH)
             notificationManager.createNotificationChannel(notificationChannel)
-
         }
 
-        notificationManager.notify(0,builder.build())
-
+        notificationManager.notify(0, builder.build())
     }
 }
