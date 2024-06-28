d947d93f9c2d22733b79385310c5f9ee6665eb96
Gaurav-822 <gaurav.moocs@gmail.com>
2024-06-28 14:57:06 +0530

    dev m ka kaam thik karna

--- app/src/main/java/com/example/deliveryapp/MainActivity.kt
+++ app/src/main/java/com/example/deliveryapp/MainActivity.kt
@@ -20,38 +20,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-
-//        bottomNavigationView = findViewById(R.id.bottom_navigation)
-
-//        bottomNavigationView.setOnItemSelectedListener { menuItem ->
-//            when (menuItem.itemId) {
-//                R.id.bottom_home -> {
-//                    // Replace fragment with HomeFragment
-//                    replaceFragment(HomeFragment())
-//                    true
-//                }
-//                R.id.bottom_cart -> {
-//                    // Replace fragment with HomeFragment
-//                    replaceFragment(CartFragment())
-//                    true
-//                }
-//                R.id.bottom_search -> {
-//                    // Replace fragment with HomeFragment
-//                    replaceFragment(SearchFragment())
-//                    true
-//                }
-//                R.id.bottom_profile -> {
-//                    // Replace fragment with HomeFragment
-//                    replaceFragment(ProfileFragment())
-//                    true
-//                }
-//                else -> false
-//            }
-//        }
-//        replaceFragment(HomeFragment())
-
     }
-    private fun replaceFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit()
-    }
+
 }
