5eae939a1c40d5ed265f2222e22f6cc924f839da
Vishal Singh <vishal684pratap@gmial.com>
2024-07-02 16:51:16 +0530

    Explore Page

--- app/src/main/java/com/example/explorepage/ExploreActivity.kt
+++ app/src/main/java/com/example/explorepage/ExploreActivity.kt
@@ -2,10 +2,38 @@ package com.example.explorepage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.example.explorepage.aFragments.PagerAdapter
+import com.google.android.material.tabs.TabLayout
 
 class ExploreActivity : AppCompatActivity() {
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager: ViewPager2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_explore)
+
+        tabLayout = findViewById(R.id.tabLayout)
+        viewPager = findViewById(R.id.ViewPager)
+
+        val pagerAdapter = PagerAdapter(supportFragmentManager, lifecycle)
+        viewPager.adapter = pagerAdapter
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.shops))
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.dishes))
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                viewPager.currentItem = tab!!.position
+
+    }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+        })
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                tabLayout.selectTab(tabLayout.getTabAt(position))
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/example/explorepage/aFragments/PagerAdapter.kt
@@ -0,0 +1,17 @@
+package com.example.explorepage.aFragments
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class PagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) : FragmentStateAdapter(fragmentManager, lifecycle) {
+    override fun getItemCount(): Int = 2
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> ShopsFragment()
+            1 -> DishesFragment()
+            else -> throw RuntimeException("Invalid position")
+        }
+    }
+}
