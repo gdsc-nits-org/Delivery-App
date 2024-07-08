04d533742c8a722e66577e2b79a005b4359ada9e
AnKiTk203 <kumar0ankit2003@gmail.com>
2024-07-08 21:46:16 +0530

    Shops Fragment UI Fixed My Orders Integrated Shop fragment fixed

---
+++ app/src/main/java/com/example/deliveryapp/Fragments/ShopDetailsFragment.kt
@@ -0,0 +1,27 @@
+package com.example.deliveryapp.Fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.deliveryapp.R
+import com.google.firebase.database.FirebaseDatabase
+
+class ShopDetailsFragment : Fragment() {
+
+    private lateinit var db: FirebaseDatabase
+    private lateinit var firestore :
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_shop_details, container, false)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
